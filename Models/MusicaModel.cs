f32aa5f0cb23b15dc99485144820876146062d2d
Edilson Torres <edilsontor77@gmail.com>
2023-01-20 23:23:13 -0300

    Criando modelo que sera espelhado no meu DB

---
+++ Models/MusicaModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Musica.Models
+{
+    public class MusicaModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? NomeArtista { get; set; }
+
+        public string? NomeMusica { get;  set; }
+
+        public DateOnly Data { get; set; }
+    }
+}
