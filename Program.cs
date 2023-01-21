764d6453c627776a760ab4a2f17d0f40abca3161
Edilson Torres <edilsontor77@gmail.com>
2023-01-20 23:26:57 -0300

    Criando a conexão com o DB

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Musica.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Buscando as informações do meu Contexto e conectando com o Banco de Dados configurado no appsettings.json
+builder.Services.AddDbContext<MusicaDBContext>(opt => opt.UseMySql(builder.Configuration.GetConnectionString("DataBase"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DataBase"))
+    ));
+
 
 var app = builder.Build();
 
